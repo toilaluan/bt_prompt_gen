2fcfbf155f5a776573c027ba765acb2547ccfef6
Your Name <you@example.com>
2023-12-04 08:34:57 +0000

    mvp

---
+++ app.py
@@ -0,0 +1,45 @@
+from transformers import pipeline
+from fastapi import FastAPI
+import torch
+from typing import List
+import numpy as np
+import random
+from pydantic import BaseModel
+
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    return seed
+
+
+class Data(BaseModel):
+    prompt: str = "an image of"
+    seed: int = 0
+    max_length: int = 77
+    additional_params: dict = {}
+
+
+app = FastAPI()
+
+generator = pipeline(model="Gustavosta/MagicPrompt-Stable-Diffusion", device="cuda")
+
+@app.post("/prompt_generate")
+async def get_rewards(data: Data):
+    seed_everything(data.seed)
+    prompt = generator(
+        data.prompt,
+        max_length=data.max_length,
+        **data.additional_params,
+    )[0]["generated_text"]
+    print("Prompt Generated:", prompt, flush=True)
+    return {"prompt": prompt}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001)
