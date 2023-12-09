1164f565cf5e891608921cdc87ca48130b5a37f1
toilaluan <toilaluan@github.com>
2023-12-09 10:15:59 +0000

    update limiter, allowing list ips

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from transformers import pipeline
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, Depends
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
 import torch
 from typing import List
 import numpy as np
@@ -7,6 +9,9 @@ import random
 from pydantic import BaseModel
 import uvicorn
 import argparse
+import requests
+import time
+import threading
 
 
 def seed_everything(seed):
@@ -28,8 +33,16 @@ class Data(BaseModel):
 app = FastAPI()
 
 generator = pipeline(model="Gustavosta/MagicPrompt-Stable-Diffusion", device="cuda")
+@app.middleware("http")
+async def filter_allowed_ips(request: Request, call_next):
+    print(str(request.url))
+    if (request.client.host not in ALLOWED_IPS) and (request.client.host != "127.0.0.1"):
+        print(f"A unallowed ip:", request.client.host)
+        return Response(content="You do not have permission to access this resource", status_code=403)
+    response = await call_next(request)
+    return response
 
-@app.post("/prompt_generate")
+@app.post("/prompt_generate", dependencies=[Depends(RateLimiter(times=30, seconds=60))])
 async def get_rewards(data: Data):
     seed_everything(data.seed)
     prompt = generator(
@@ -40,9 +53,23 @@ async def get_rewards(data: Data):
     print("Prompt Generated:", prompt, flush=True)
     return {"prompt": prompt}
 
+def define_allowed_ips(url):
+    global ALLOWED_IPS
+    ALLOWED_IPS = []
+    while True:
+        response = requests.get(f"{url}/get_allowed_ips")
+        response = response.json()
+        ALLOWED_IPS = response['allowed_ips']
+        print("Updated allowed ips:", ALLOWED_IPS, flush=True)
+        time.sleep(60)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", type=int, default=10001)
+    parser.add_argument("--subnet_outbound_url", type=str, default="http://20.210.111.232:10005")
     args = parser.parse_args()
+    allowed_ips_thread = threading.Thread(target=define_allowed_ips, args=(args.subnet_outbound_url,))
+    allowed_ips_thread.setDaemon(True)
+    allowed_ips_thread.start()
     uvicorn.run(app, host="0.0.0.0", port=args.port)
